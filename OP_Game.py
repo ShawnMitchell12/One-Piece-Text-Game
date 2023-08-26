b24c3d0abd185ac2b4efc38c8e137a2f33ee9ea7
Shawn Mitchell <shawnmitch12@gmail.com>
2023-08-26 13:43:51 -0700

    Added line spacing during repr for pirate

--- OP_Game.py
+++ OP_Game.py
@@ -1,3 +1,12 @@
 import OP_Classes.py
 
+#Giving the captains a handful of options to choose for their pirates
+
+a = Pirate('Luffy', 'Devil Fruit', 7)
+b = Pirate('Zoro', 'Sword', 8)
+c = Pirate('Shanks', 'Haki', 10)
+d = Pirate('BlackBeard', 'Devil Fruit', 8)
+e = Pirate('Tsushigi', 'Sword', 4)
+f = Pirate('Oden', 'Sword')
+
 
